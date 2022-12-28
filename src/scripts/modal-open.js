24166646b9bdeed2b0c92a512e4a5e22513dd8eb
Vadim Moshev <reg-vadim-moshev@yandex.ru>
2022-12-28 20:59:46 +0500

    finish modal open functionality by corresponding buttons clicking

--- src/scripts/modal-open.js
+++ src/scripts/modal-open.js
@@ -7,10 +7,26 @@ const toggleScrollVisibility = (flag) => {
 
 const closePopupButon = getById("close-popup");
 const overlay = getById("overlay");
+const popup = getById("popup");
+const orderButtonHeader = getById("order-button-header");
+const orderButtonFooter = getById("order-button-footer");
+
+const openPopupHandler = () => {
+  alert("хай!");
+};
 
 const closePopupHandler = () => {
   overlay.classList.remove("modal-overlay--visible");
   toggleScrollVisibility(true);
 };
+
+orderButtonHeader?.addEventListener("click", openPopupHandler);
+orderButtonFooter?.addEventListener("click", openPopupHandler);
+
+overlay?.addEventListener("click", closePopupHandler);
+
+popup?.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
 
 closePopupButon?.addEventListener("click", closePopupHandler);
